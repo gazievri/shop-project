fc56dc4a471054fea48bb55f18fba57c58771d0a
Ruslan Gaziev <grinrus@yandex.ru>
2022-07-14 20:42:30 +0300

    Header and footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
-    <footer className="page-footer green lighten-2">
+    <footer className="page-footer light-blue darken-4">
       <div className="footer-copyright">
         <div className="container">
           {`© ${new Date().getFullYear()}. Ruslan Gaziev`}
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a className="grey-text text-lighten-4 right" href="https://github.com/gazievri/shop-project">
             Repo
           </a>
         </div>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,13 @@
 function Header() {
   return (
-    <nav className="green darken-2">
+    <nav className="light-blue darken-4">
       <div className="nav-wrapper">
         <a href="#" className="brand-logo">
            My Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="#">Repo</a>
+            <a href="https://github.com/gazievri/shop-project">Repo</a>
           </li>
         </ul>
       </div>
