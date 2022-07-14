9c175a134891d6073d9a10215669590ee08f2201
Ruslan Gaziev <grinrus@yandex.ru>
2022-07-14 18:17:44 +0300

    Start project

---
+++ src/layout/Shop.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Shop = () => {
+  return(
+    <main className="container content"></main>
+  )
+}
+
+export default Shop;
---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_URL = 'https://fortniteapi.io/v2/shop?lang=en';
