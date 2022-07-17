54b7c4de048c47f1b38573c3f5c90992cf171ae1
Ruslan Gaziev <grinrus@yandex.ru>
2022-07-17 17:48:57 +0300

    gh-pages were added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,6 +7,8 @@ function Footer() {
           <a
             className="grey-text text-lighten-4 right"
             href="https://github.com/gazievri/shop-project"
+            target="_blank"
+            rel="noreferrer"
           >
             Repo
           </a>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
   return (
     <nav className="light-blue darken-4">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
-          My Shop
+        <a href="#!" className="brand-logo">
+          Your Best Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="https://github.com/gazievri/shop-project">Repo</a>
+            <a
+              href="https://github.com/gazievri/shop-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
